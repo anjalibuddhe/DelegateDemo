b2aa8c39087d6cd299ae704603da649a4754f179
anjali buddhe <anjalibuddhe124@gmail.com>
2023-07-24 17:19:12 +0530

    Add project files.

---
+++ DelegateDemo/Calculation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateDemo
+{
+
+    public delegate int MyDelegate(int x,int y);
+    public class Calculation
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public int  Sub(int x, int y)
+        {
+            return x - y;
+        }
+    }
+
+    public delegate void Delegate1(int id, string name, double salary);
+
+    public class Test
+    {
+        public void AcceptDetails(int id ,string name,double salary)
+        {
+            Console.WriteLine($"{id} {name} {salary}");
+        }
+    }
+}
---
+++ DelegateDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculation c=new Calculation();
+            //c.Add
+            MyDelegate myDelegate = new MyDelegate(c.Add);
+            //Console.WriteLine(myDelegate.Invoke(20,30));
+
+            myDelegate += new MyDelegate(c.Sub);
+
+            //myDelegate -= new MyDelegate(c.Sub);
+
+            Delegate[] list = myDelegate.GetInvocationList();
+
+            foreach(Delegate item in list)
+            {
+                Console.WriteLine(item.Method);
+                Console.WriteLine(item.DynamicInvoke(20,10));
+            }
+
+            ////Test t1=new Test();
+            ////Delegate1 d1 = new Delegate1(t1.AcceptDetails);
+            ////d1.Invoke(1, "sara", 25000);
+        }
+    }
+}
